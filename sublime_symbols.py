240d761f19d86ff95a99a89ab1924abf304e4a00
Luis Ángel Méndez Gort <lume@simcorp.com>
2024-05-15 17:28:31 +0200

    support for replacing sequences of arbitrary length at the end of the line

--- sublime_symbols.py
+++ sublime_symbols.py
@@ -3,22 +3,59 @@ import sublime_plugin
 import json
 import os
 
+
+def enabled_symbol_packages():
+    settings = sublime.load_settings("SublimeSymbols.sublime-settings")
+    symbol_packages = settings.get("enabled_symbol_packages", [])
+    if symbol_packages == None:
+        symbol_packages = ["wybe"]
+
+    return symbol_packages
+
+
+def load_symbol_packages(symbol_packages):
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    symbol_packages_dir = os.path.join(current_dir, "packages")
+    symbol_map = {}
+    for package in symbol_packages:
+        file_path = os.path.join(symbol_packages_dir, package + ".json")
+
+        with open(file_path, "r", encoding="utf-8") as f:
+            symbol_map = json.load(f)
+
+        for key, value in symbol_map.items():
+            symbol_map[key] = value
+
+    return symbol_map
+
+
+class ReplaceStringCommand(sublime_plugin.TextCommand):
+    def run(self, edit, start, end, text):
+        self.view.replace(edit, sublime.Region(start, end), text)
+
+
 class ReplaceListener(sublime_plugin.EventListener):
     def __init__(self):
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-
-        # Define the file path
-        file_path = os.path.join(current_dir, 'packages', 'wybe.json')
-
-        with open(file_path, 'r', encoding='utf-8') as f:
-            self.current_map = json.load(f)
-            
+        enabled = enabled_symbol_packages()
+        print(enabled)
+        self.current_map = load_symbol_packages(enabled)
 
     def on_modified(self, view):
-        cursor_pos = view.sel()[0].begin()
+        point = view.sel()[0].begin()
+        # Get the row and column of the current cursor position
+        row, col = view.rowcol(point)
 
-        last_two_chars = view.substr(sublime.Region(cursor_pos - 2, cursor_pos))
-        
-        if last_two_chars in self.current_map.keys():
-            view.run_command('undo')
-            view.run_command('insert', {'characters': self.current_map[last_two_chars]})
+        # Get the line text
+        line_region = view.line(point)
+        current_line = view.substr(line_region)
+
+        for suffix, replacement in self.current_map.items():
+            if current_line.endswith(suffix):
+                view.run_command(
+                    "replace_string",
+                    {
+                        "start": line_region.end() - len(suffix),
+                        "end": line_region.end(),
+                        "text": replacement,
+                    },
+                )
